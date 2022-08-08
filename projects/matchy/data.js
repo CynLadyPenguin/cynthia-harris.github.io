50dc1f1c86d5ebc49762abf312047f8af3d99495
CynLadyPenguin <ladypenguin1991@gmail.com>
2022-08-08 22:25:17 +0000

    Matchy

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -82,12 +82,12 @@ console.log(animals);
 var friends = [];
 //I didn't choose it. The data structure chose me. Listing strings is better in an array when you don't need your key value pairs.
 function getRandom(animals){
-  return Math.floor(Math.random(animals.name));
-  friends.push(animals.name[random]);
+  var random = Math.floor(Math.random() * animals.length);
+  friends.push(random);
 }
 console.log(friends);
 animals.duck["friends"] = friends;
-
+console.log(duck);
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
  * move onto Part 2 in the file called "functions.js"
