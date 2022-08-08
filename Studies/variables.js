46ff2f1e93b350d76c4c4716d6bf286290ba867b
CynLadyPenguin <ladypenguin1991@gmail.com>
2022-08-08 17:02:00 +0000

    Variable study guide

--- Studies/variables.js
+++ Studies/variables.js
@@ -1,7 +1,26 @@
 /**
- *VARIABLES 
- * Text and code examples for these study guides
- * 0. Variables are named containers that hold data in Javascript. They can hold
+ *VARIABLES
+ * 
+ *  Variables are containers for storing data and data values. They are named and can hold things like objects, arrays, functions, expressions, statements, numbers, 
+ * booleans, strings, etc. The names of variables are called "identifiers". Identifiers are case sensitive. Below we'll detail declaring and assignment, var/let/const, and 
+ * hoisting:
+ * 
+ * 0. DECLARATION AND ASSIGNMENT
+ * 
+ * To declare a variable you need to choose a key word. For a standard variable you would use the keyword "var", followed by the unique name of the variable,
+ * and then an = operator. Lastly, to assign a value to your variable, you would add the value that you want to store in your new named container. 
+ * Typically, a person would use camelCase where the first word is lowercase and the second word is capitalized like in the example below. 
+ * If there is only one word then it should be lowercase. The variable value should always end in a semicolon. If there's no value, put the semicolon behind the name of the variable.
+ * These are the steps to declaring and assigning a variable:
+ * 
+ *  var 
+ *  var uniqueName
+ *  var uniqueName =
+ *  var uniqueName = true; //This declared variable has a value of true. If logged to the console (console.log(uniqueName)) the return will be the word "true".
+ * 
+ * 1. VAR, LET, CONST
+ * 
+ * 
  * 
  * 
  * 
