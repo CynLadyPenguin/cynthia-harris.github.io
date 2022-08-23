0c87a9123ff79c29bcc4373a090c219af4e57047
CynLadyPenguin <ladypenguin1991@gmail.com>
2022-08-23 15:56:00 +0000

    recursion

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -288,17 +288,42 @@ var fibonacci = function(n) {
 // nthFibo(7); // 13
 // nthFibo(3); // 2
 var nthFibo = function(n) {
+  //base
+  if(n < 0){
+    return null;
+  }
+  if(n === 0){
+    return 0;
+  }
+  if(n === 1){
+    return 1;
+  }
+  return nthFibo(n-1) + nthFibo(n-2);
 };
 
 // 26. Given an array of words, return a new array containing each word capitalized.
 // var words = ['i', 'am', 'learning', 'recursion'];
 // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
 var capitalizeWords = function(input) {
+  //base
+  if(input.length === 0){
+    return input;
+  }
+  var capitals = capitalizeWords(input.slice(1, input.length));
+  capitals.unshift(input[0].toUpperCase());
+  return capitals;
 };
 
 // 27. Given an array of strings, capitalize the first letter of each index.
 // capitalizeFirst(['car', 'poop', 'banana']); // ['Car', 'Poop', 'Banana']
 var capitalizeFirst = function(array) {
+    //base
+    if(array.length === 0){
+      return [];
+    }
+    var capital = capitalizeFirst(array.slice(1, array.length));
+    capital.unshift(array[0][0].toUpperCase() + array[0].substring(1));
+    return capital;
 };
 
 // 28. Return the sum of all even numbers in an object containing nested objects.
@@ -320,7 +345,18 @@ var flatten = function(arrays) {
 
 // 30. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {'p':1, 'o':2, 't':2, 'a':1}
-var letterTally = function(str, obj) {
+var letterTally = function(str, obj = {}) {
+  //base
+  if (str.length === 0){
+     return obj;
+  }
+  letterTally(str.substring(1), obj);
+  if (obj[str[0]] === undefined) {
+    obj[str[0]] = 1;
+  } else {
+    obj[str[0]] += 1;
+  }
+  return obj;
 };
 
 // 31. Eliminate consecutive duplicates in a list.  If the list contains repeated
@@ -329,6 +365,14 @@ var letterTally = function(str, obj) {
 // Example: compress([1, 2, 2, 3, 4, 4, 5, 5, 5]) // [1, 2, 3, 4, 5]
 // Example: compress([1, 2, 2, 3, 4, 4, 2, 5, 5, 5, 4, 4]) // [1, 2, 3, 4, 2, 5, 4]
 var compress = function(list) {
+  if(list.length === 0){
+    return list;
+  }
+  var rest = compress(list.slice(1));
+  if (list[0] !== rest[0]) {
+    rest.unshift(list[0]);
+  }
+  return rest;
 };
 
 // 32. Augment every element in a list with a new value where each element is an array
@@ -341,6 +385,14 @@ var augmentElements = function(array, aug) {
 // minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
 // minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
 var minimizeZeroes = function(array) {
+  if(array.length === 0){
+    return array;
+  }
+  var list = minimizeZeroes(array.slice(1));
+  if ((array[0] === 0 ^ list[0] === 0) || array[0] !== 0) {
+    list.unshift(array[0]);
+  }
+  return list;  
 };
 
 // 34. Alternate the numbers in an array between positive and negative regardless of
@@ -348,12 +400,56 @@ var minimizeZeroes = function(array) {
 // alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
 // alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
 var alternateSign = function(array) {
+  if (array.length === 0){
+     return [];
+  }
+  var list = alternateSign(array.slice(0, array.length-1));
+  var lng = array.length;
+  if (lng %2 === 0) {
+    if (array[lng - 1] > 0) {
+      array[lng-1] = -array[lng-1];
+    }
+  } else {
+    if (array[lng - 1] < 0) {
+      array[lng - 1] = -array[lng - 1];
+    }
+  }
+  list.push(array[lng - 1]);
+  return list;
 };
 
 // 35. Given a string, return a string with digits converted to their word equivalent.
 // Assume all numbers are single digits (less than 10).
 // numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
 var numToText = function(str) {
+  if (str.length === 0){
+     return '';
+  }
+  var tempStr = numToText(str.substring(0, str.length - 1));
+  var replace;
+  switch (str[str.length - 1]) {
+    case '1': replace = 'one';
+      break;
+    case '2': replace = 'two';
+      break;
+    case '3': replace = 'three';
+      break;
+    case '4': replace = 'four';
+      break;
+    case '5': replace = 'five';
+      break;
+    case '6': replace = 'six';
+      break;
+    case '7': replace = 'seven';
+      break;
+    case '8': replace = 'eight';
+      break;
+    case '9': replace = 'nine';
+      break;
+    default: replace = str[str.length - 1];
+      break;
+  }
+  return tempStr + replace;
 };
 
 // *** EXTRA CREDIT ***
